61260e9a4baaed3c3d1c907ddb89b9fc8b8fc3aa
Virendra prajapat <virendraprajapat9289@gmail.com>
2025-03-27 16:21:34 +0530

    backend

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  logOut,
+} = require("../controllers/authController");
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logOut);
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const authRouter = require("./routes/authRoutes");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+//middelwares
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+//DB connection
+connectDB();
+
+app.use("/api/auth", authRouter);
+
+app.listen(PORT, () => console.log(`Server running at PORT: ${PORT}`));
