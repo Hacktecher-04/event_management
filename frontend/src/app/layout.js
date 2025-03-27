feabe6dc5c54185b3e8b31e0313858ef0c438cff
rosekushwah <kroshni9977@gmail.com>
2025-03-27 19:57:18 +0530

    Add Header in layout

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -10,9 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <Header />
         {children}
       </body>
     </html>
