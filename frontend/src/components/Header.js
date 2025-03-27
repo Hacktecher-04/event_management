03f7f8aab450f57351e4e30a14d84496c7654e32
Manoj Charan <manojcharan1525@gmail.com>
2025-03-27 21:22:08 +0530

    navbar designed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav className="p-5 text-xl bg-white text-black flex justify-between">
+      <h1 className='text-2xl'>Logo</h1>
+      <ul className="flex w-xs justify-between">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/events">Events</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contacts</Link>
+        </li>
+      </ul>
+      <div >
+        <ul className='flex justify-around w-50 text-white'>
+        <li className='bg-blue-500 w-20 h-8 flex justify-around'>
+          <Link href="/auth/login">
+            <button>Login</button>
+          </Link>
+        </li>
+
+        <li className='bg-blue-600 w-25 h-8 flex justify-around'>
+          <Link href="/auth/register">
+            <button>Register</button>
+          </Link>
+        </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Header
