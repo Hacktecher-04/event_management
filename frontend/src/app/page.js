6ad8d5601771049704b4940e6673c0d7b9c5913e
rosekushwah <kroshni9977@gmail.com>
2025-03-27 16:26:41 +0530

    second change

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-7xl sm:text-9xl md:text-[120px] lg:text-[150px] xl:text-[180px] font-bold text-center">
-        Home page
+        Home page 123
       </h1>
     </div>
   );
